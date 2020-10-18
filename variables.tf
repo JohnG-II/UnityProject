153512b7aed908f3e997f37fd79ed82009b6af32
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2020-10-17 21:39:22 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "cluster-name" {
   type    = string
 }
 variable "key_pair_name" {
-  default = "canada"
+  default = "class21"
 }
 variable "eks_node_instance_type" {
   default = "t2.medium"
